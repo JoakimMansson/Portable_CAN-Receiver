3ab5cfb2a10622c3265b5233bea72a195773998f
Whit3eo <jockel121.121@gmail.com>
2022-06-27 22:25:42 +0200

    mby this works?

--- RPi_screen.py
+++ RPi_screen.py
@@ -1,5 +1,7 @@
 import tkinter
 from tkinter import *
+from tkinter.ttk import Panedwindow
+
 from PIL import Image, ImageTk
 
 
@@ -9,55 +11,59 @@ class test(Tk):
         super().__init__()
 
         #Setting dimensions and resizability
-        self.geometry("500x500")
+        self.geometry("800x480")
         self.resizable(False,False)
         self.title("CAN debugger")
         self.img = ImageTk.PhotoImage(Image.open(("HUST_logo.png")).resize((100, 100)))
 
-        self.receive_canvas = Canvas(self, width=500,height=500)
-        self.send_canvas = Canvas(self, width=500, height=500)
+        self.receive_frame = Frame(self, width=800, height=480)
+        self.send_frame = Frame(self, width=800, height=480)
+
+
+        self.receive_screen()
+
+        self.receive_frame.pack()
+
 
 
     def receive_screen(self):
-        #self.send_canvas.delete("all")
-        #self.
 
-        self.receive_canvas.create_image(250, 100, image=self.img)
 
-        send_btn = Button(self, text="Send CAN", command=lambda: self.send_screen())
-        send_btn.pack(side=BOTTOM, padx=10, pady=10)
+        receiving_can = Label(self.receive_frame, text="Send Can", font=("Arial", 25))
+        receiving_can.place(relx=0.5, rely=0.3, anchor="center")
 
-        self.receive_canvas.create_text(250,200, text="Receiving CAN", fill="black", font=("Arial",25))
+        # BMS
+        pack_current = Label(self.receive_frame, text="pack current:", font=("Arial", 15))
+        pack_current.place(relx=0.3, rely=0.45, anchor="center")
 
-        #BMS
-        self.receive_canvas.create_text(125, 275, text="pack current:", font=("Arial", 15))
-        self.receive_canvas.create_text(125, 325, text="pack voltage:", font=("Arial", 15))
-        self.receive_canvas.create_text(125, 375, text="temp BMS:", font=("Arial", 15))
-        self.receive_canvas.create_text(125, 425, text="cell voltage:", font=("Arial", 15))
+        pack_voltage = Label(self.receive_frame, text="pack voltage:", font=("Arial", 15))
+        pack_voltage.place(relx=0.3, rely=0.57, anchor="center")
 
-        #MC
-        self.receive_canvas.create_text(350, 275, text="velocity ms:", font=("Arial", 15))
-        self.receive_canvas.create_text(350, 325, text="velocity rpm:", font=("Arial", 15))
-        self.receive_canvas.create_text(350, 375, text="heatsink temp", font=("Arial", 15))
-        self.receive_canvas.create_text(350, 425, text="motor temp:", font=("Arial", 15))
+        temp_BMS = Label(self.receive_frame, text="temp BMS:", font=("Arial", 15))
+        temp_BMS.place(relx=0.3, rely=0.69, anchor="center")
 
-        self.receive_canvas.pack()
+        cell_voltage = Label(self.receive_frame, text="cell voltage:", font=("Arial", 15))
+        cell_voltage.place(relx=0.3, rely=0.81, anchor="center")
+
+        # MC
+        velocity_ms = Label(self.receive_frame, text="velocity ms:", font=("Arial", 15))
+        velocity_ms.place(relx=0.7, rely=0.45, anchor="center")
+
+        velocity_rpm = Label(self.receive_frame, text="velocity rpm:", font=("Arial", 15))
+        velocity_rpm.place(relx=0.7, rely=0.57, anchor="center")
+
+        heatsink_temp = Label(self.receive_frame, text="heatsink temp", font=("Arial", 15))
+        heatsink_temp.place(relx=0.7, rely=0.69, anchor="center")
+
+        motor_temp = Label(self.receive_frame, text="motor temp:", font=("Arial", 15))
+        motor_temp.place(relx=0.7, rely=0.81, anchor="center")
 
 
     def send_screen(self):
-        #self.receive_canvas.delete("all")
-
-        self.send_canvas.create_image(250, 100, image=self.img)
-
-        #send_btn = Button(self, text="Receive CAN", command=lambda: self.receive_screen())
-       # send_btn.pack(side=BOTTOM, padx=10, pady=10)
-
-        self.send_canvas.create_text(250,200, text="Sending CAN", fill="black", font=("Arial",25))
-
-        #self.send_canvas.pack()
+        pass
 
 
 if __name__ == "__main__":
     t = test()
-    t.receive_screen()
+    #t.receive_screen()
     t.mainloop()
