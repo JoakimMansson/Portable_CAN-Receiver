abe7e7d66527cba0ea3ce2dc6205fae2c310c988
Whit3eo <jockel121.121@gmail.com>
2022-08-13 18:32:13 +0200

    ddd

--- RPi_GUI.py
+++ RPi_GUI.py
@@ -31,124 +31,58 @@ from kivy.core.window import Window
 
 
 
-class Gauge(Widget):
-
-    unit = NumericProperty(1.8)
-    value = BoundedNumericProperty(0, min=0, max=100, errorvalue=0)
-    path = dirname(abspath(__file__))
-    file_gauge = StringProperty(join(path, "cadran.png"))
-    file_needle = StringProperty(join(path, "needle.png"))
-    size_gauge = BoundedNumericProperty(128, min=128, max=256, errorvalue=128)
-    size_text = NumericProperty(10)
-
-    def __init__(self, **kwargs):
-        super(Gauge, self).__init__(**kwargs)
-
-        self._gauge = Scatter(
-            size=(1350,600),
-            do_rotation=False,
-            do_scale=False,
-            do_translation=False
-        )
-
-        _img_gauge = Image(
-            source=self.file_gauge,
-            size=(1350, 600)
-
-        )
-
-        self._needle = Scatter(
-            size=(self.size_gauge, self.size_gauge),
-            do_rotation=False,
-            do_scale=False,
-            do_translation=False
-        )
-
-        _img_needle = Image(
-            source=self.file_needle,
-            size=(self.size_gauge, self.size_gauge)
-        )
-
-        self._glab = Label(font_size=self.size_text, markup=True, color=(0, 0, 0, 1))
-
-        self._gauge.add_widget(_img_gauge)
-        self._needle.add_widget(_img_needle)
-
-        self.add_widget(self._gauge)
-        self.add_widget(self._needle)
-        self.add_widget(self._glab)
-        self.bind(pos=self._update)
-        self.bind(size=self._update)
-        self.bind(value=self._turn)
-
-    def _update(self, *args):
-        '''
-        Update gauge and needle positions after sizing or positioning.
-        '''
-        self._gauge.pos = self.pos
-        self._needle.pos = (self.x, self.y)
-        self._needle.center = self._gauge.center
-        self._glab.center_x = self._gauge.center_x
-        self._glab.center_y = self._gauge.center_y + (self.size_gauge / 4)
-
-    def _turn(self, *args):
-        '''
-        Turn needle, 1 degree = 1 unit, 0 degree point start on 50 value.
-        '''
-        self._needle.center_x = self._gauge.center_x
-        self._needle.center_y = self._gauge.center_y
-        self._needle.rotation = (50 * self.unit) - (self.value * self.unit)
-        self._glab.text = "[b]{0:.0f}[/b]".format(self.value)
-
-
-class StartScreen(Screen):
-
-    def __init__(self, **kwargs):
-        super(StartScreen, self).__init__(**kwargs)
-        Clock.schedule_once(lambda dt: self.do_something(), 1 / 60)
-
-
-    def do_something(self):
-        self.manager.current = "Main"
-
 
 class MainScreen(Screen):
     neutralB = ObjectProperty(None)
     reverseB = ObjectProperty(None)
     driveB = ObjectProperty(None)
-    current_status = ObjectProperty(None)
+    current_speed = ObjectProperty(None)
+
+    bms_temp_img = ObjectProperty(None)
+    motor_temp_img = ObjectProperty(None)
+
 
     def __init__(self, **kw):
         super().__init__(**kw)
-        self.gauge = Gauge(value=50,
-                           size_gauge=1000,
-                           size_text=50,
-                           pos_hint={"center_x": 0.18, "center_y": 0.3}
-                           )
-        self.add_widget(self.gauge)
+
 
 
     def set_current_velocity(self, velocity):
-        self.gauge.value = velocity
+        self.current_speed.text = str(velocity)
 
 
     def reverse(self):
-        self.reverseB.disabled = True
-        self.driveB.disabled = False
-        self.neutralB.disabled = False
-        self.current_status.text = "Reverse"
+        self.reverseB.line_color = (0/255, 109/255, 176/255, 1)
+        self.reverseB.text_color = (0/255, 109/255, 176/255, 1)
+
+        self.driveB.line_color = (1, 1, 1, 1)
+        self.driveB.text_color = (1, 1, 1, 1)
+
+        self.neutralB.line_color = (1, 1, 1, 1)
+        self.neutralB.text_color = (1, 1, 1, 1)
+
 
     def drive(self):
-        self.driveB.disabled = True
-        self.reverseB.disabled = False
-        self.neutralB.disabled = False
-        self.current_status.text = "Drive"
+        self.driveB.line_color = (0 / 255, 109 / 255, 176 / 255, 1)
+        self.driveB.text_color = (0 / 255, 109 / 255, 176 / 255, 1)
+
+        self.reverseB.line_color = (1, 1, 1, 1)
+        self.reverseB.text_color = (1, 1, 1, 1)
+
+        self.neutralB.line_color = (1, 1, 1, 1)
+        self.neutralB.text_color = (1, 1, 1, 1)
+
 
     def neutral(self):
-        self.neutralB.disabled = True
-        self.driveB.disabled = False
-        self.reverseB.disabled = False
-        self.current_status.text = "Neutral"
+        self.neutralB.line_color = (0 / 255, 109 / 255, 176 / 255, 1)
+        self.neutralB.text_color = (0 / 255, 109 / 255, 176 / 255, 1)
+
+        self.reverseB.line_color = (1, 1, 1, 1)
+        self.reverseB.text_color = (1, 1, 1, 1)
+
+        self.driveB.line_color = (1, 1, 1, 1)
+        self.driveB.text_color = (1, 1, 1, 1)
+
 
 
 class WindowManager(ScreenManager):
